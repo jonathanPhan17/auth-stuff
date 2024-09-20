19549bef8189eafb0b5bb4a55a5aaefaadde196c
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-20 16:37:04 -0700

    implement user registration and JWT authentication

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
 
 export const signup = async (req, res) => {
     const { email, password, name } = req.body;
@@ -28,8 +29,16 @@ export const signup = async (req, res) => {
       await user.save();
 
       //jwt
-      generateTokenAndSetCookie(res, user._id)
+      generateTokenAndSetCookie(res, user._id);
 
+      res.status(201).json({ 
+        success: true, 
+        message: "user created successfully", 
+        user: {
+          ...user._doc,
+          password: undefined,
+      },
+    })
     } catch (error) {
         res.status(400).json({ success: false, message: error.message });
     }
