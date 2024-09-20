f42e30d35b1109ad91d53521a43d1636c5f28211
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-20 12:09:42 -0700

    fix route by importing route and changing route name

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { connectDB } from './db/db.js';
 import dotenv from "dotenv";
+import Router from './routes/auth.route.js';
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.get("/", (req, res) => {
     res.send("sup")
 })
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", Router);
 
 app.listen(3000, () => {
     connectDB();
