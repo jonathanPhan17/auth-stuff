774cb6193a977aad5743c6472e36f79b89bd3bd3
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-20 11:53:01 -0700

    fix db connection by adding dotenv config to index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { connectDB } from './db/db.js';
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
 
 app.get("/", (req, res) => {
     res.send("sup")
