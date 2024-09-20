5abce3ee0b706cec5289e018bda190fc16937d68
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-20 11:56:34 -0700

    create auth routes and folder with express

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ app.get("/", (req, res) => {
     res.send("sup")
 })
 
+app.use("/api/auth", authRoutes);
+
 app.listen(3000, () => {
     connectDB();
     console.log("Server running on port 3000")
