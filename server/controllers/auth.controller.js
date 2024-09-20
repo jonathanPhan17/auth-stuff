0e5741452a2e46f77291d1593da3d62846ebd3c9
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-20 12:16:25 -0700

    extract login function from auth.route.js and move to auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,3 +1,7 @@
 export const signup = async (req, res) => {
     res.send("Signup Route");
 }
+
+export const login = async (req, res) => {
+    res.send("Login Route");
+}
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,13 +1,11 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller';
+import { login, signup } from '../controllers/auth.controller';
 
 const Router = express.Router();
 
 Router.get("/signup", signup);
 
-Router.get("/login", (req, res) => {
-    res.send("Login Route")
-})
+Router.get("/login", login);
 
 Router.get("/logout", (req, res) => {
   res.send("Logout Route");
