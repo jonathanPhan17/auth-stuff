b3595e299c6a9e72d1982f8e6c5fc959a113263a
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-22 17:05:59 -0700

    add bg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,43 @@
-function App() {
+import FloatinStuff from './components/FloatinStuff'
 
+function App() {
   return (
     <>
-      <h1 className="text-red-500 text-5xl">Auth Stuff</h1>
+      <div
+        className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-sky-900
+ flex items-center justify-center relative overflow-hidden"
+      >
+        <FloatinStuff
+          color="bg-blue-500"
+          size="w-64 h-64"
+          top="-5%"
+          left="10%"
+          delay={0}
+        />
+        <FloatinStuff
+          color="bg-sky-500"
+          size="w-64 h-64"
+          top="50%"
+          left="80%"
+          delay={4}
+        />
+        <FloatinStuff
+          color="bg-blue-500"
+          size="w-64 h-64"
+          top="40%"
+          left="-10%"
+          delay={2}
+        />
+        <FloatinStuff
+          color="bg-blue-500"
+          size="w-64 h-64"
+          top="-10%"
+          left="50%"
+          delay={2}
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
