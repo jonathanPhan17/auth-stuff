0a20b0082f715ff96408e3ade2d68602eed13bbc
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-22 20:48:44 -0700

    add pages layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
-import FloatinStuff from './components/FloatinStuff'
+import FloatinStuff from './components/FloatinStuff';
+import { Routes, Route } from 'react-router-dom';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
@@ -16,25 +20,30 @@ function App() {
         />
         <FloatinStuff
           color="bg-sky-500"
-          size="w-64 h-64"
+          size="w-48 h-48"
           top="50%"
           left="80%"
           delay={4}
         />
         <FloatinStuff
           color="bg-blue-500"
-          size="w-64 h-64"
+          size="w-48 h-48"
           top="40%"
           left="-10%"
           delay={2}
         />
         <FloatinStuff
           color="bg-blue-500"
-          size="w-64 h-64"
+          size="w-48 h-48"
           top="-10%"
           left="50%"
           delay={2}
         />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
       </div>
     </>
   );
