cd3553fd53b924172f80e20ca2637d3c008567d6
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-24 18:45:24 -0700

    enable cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { connectDB } from './db/db.js';
 import dotenv from "dotenv";
+import cors from "cors";
 import Router from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({origin:"http://localhost:5173", credentials: true}));
 
 app.use(express.json());
 app.use(cookieParser());
