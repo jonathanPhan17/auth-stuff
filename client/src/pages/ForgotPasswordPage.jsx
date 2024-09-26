60e12f4de32dcd7b9ddc430787ba9c4a2a708027
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-26 11:35:29 -0700

    create forgot password page component

--- client/src/pages/ForgotPasswordPage.jsx
+++ client/src/pages/ForgotPasswordPage.jsx
@@ -1,17 +1,92 @@
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-import { useAuthStore } from '../store/AuthStore'
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { useAuthStore } from "../store/AuthStore";
+import { ArrowLeft, Mail, Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+import Input from "../components/Input";
 
 const ForgotPasswordPage = () => {
-
   const [email, setEmail] = useState("");
-  const [isSubmited, setIsSubmitted] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState("");
 
-  const { isLoading, forogotPassword } = useAuthStore();
+  const { isLoading, forgotPassword } = useAuthStore();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await forgotPassword(email);
+    setIsSubmitted(true);
+  };
 
   return (
-    <div>ForgotPasswordPage</div>
-  )
-}
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-blue-400 to-sky-500 text-transparent bg-clip-text">
+          Forgot Password
+        </h2>
 
-export default ForgotPasswordPage
+        {!isSubmitted ? (
+          <form onSubmit={handleSubmit}>
+            <p className="text-gray-300 mb-6 text-center">
+              Enter your email address and we'll send you a link to reset your
+              password
+            </p>
+            <Input
+              icon={Mail}
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <motion.button
+              className="w-full py-3 px-4 bg-sky-600 text-white font-bold rounded-lg shadow-lg 
+            hover:bg-blue-700 hover:shadow-2xl focus:outline-none focus:ring-2 focus:ring-blue-600 
+            focus:ring-offset-2 focus:ring-offset-gray-900 transition-all duration-100 ease-linear"
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
+              type="submit"
+            >
+              {isLoading ? (
+                <Loader className="size-6 animate-spin mx-auto" />
+              ) : (
+                "Send Reset Link"
+              )}
+            </motion.button>
+          </form>
+        ) : (
+          <div className="text-center">
+            <motion.div
+              initial={{ scale: 0 }}
+              animation={{ scale: 1 }}
+              transition={{ type: "spring", stiffness: 500, damping: 30 }}
+              className="w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center mx-auto mb-4"
+            >
+              <Mail className="h-8 w-8 text-white" />
+            </motion.div>
+            <p className="text-gray-300 mb-6">
+              If an account exists for {email}, you will recieve a password
+              reset link shortly.
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+        <Link
+          to={"/login"}
+          className="text-sm text-blue-400 hover:underline flex items-center"
+        >
+          <ArrowLeft className="h-4 w-4 mr-2" />
+          Back to Login
+        </Link>
+      </div>
+    </motion.div>
+  );
+};
+
+export default ForgotPasswordPage;
