fb0daf40aa135e7d8df997c6792308504ee43e01
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-26 12:06:20 -0700

    implement reset password func for user auth

--- client/src/store/AuthStore.js
+++ client/src/store/AuthStore.js
@@ -11,6 +11,7 @@ export const useAuthStore = create((set) => ({
   error: null,
   isLoading: false,
   isCheckingAuth: true,
+  message: null
 
   signup: async (email, password, name) => {
     set({ isLoading: true, error: null });
@@ -113,5 +114,17 @@ export const useAuthStore = create((set) => ({
 
   resetPassword: async (token, password) => {
     set({ isLoading: true, error: null })
+
+    try {
+      const response = await axios.post(`${API_URL}/reset-password/${token}`, { password });
+      set({ message: response.data.message, isLoading: false });
+
+    } catch (error) {
+      set({
+        isLoading: false,
+        error: error.response.data.message || "Error resetting password",
+      });
+      throw error;
+    }
   }
 }));
