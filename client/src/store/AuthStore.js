b244421686b19e4880fed49c46894237d2ec54c2
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-26 12:07:35 -0700

    quick fix in auth

--- client/src/store/AuthStore.js
+++ client/src/store/AuthStore.js
@@ -11,7 +11,7 @@ export const useAuthStore = create((set) => ({
   error: null,
   isLoading: false,
   isCheckingAuth: true,
-  message: null
+  message: null,
 
   signup: async (email, password, name) => {
     set({ isLoading: true, error: null });
