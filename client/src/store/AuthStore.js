4bacbd5e4d6e202885cb3d297718a6ac04838182
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-28 23:42:26 -0700

    replace local url with render url

--- client/src/store/AuthStore.js
+++ client/src/store/AuthStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://auth-stuff-node.onrender.com";
 
 axios.defaults.withCredentials = true;
 
