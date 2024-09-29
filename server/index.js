153822c55669123d96e885171e8b9f827703261d
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-29 14:58:09 -0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
-  cors({ origin: "https://auth-stuff-seven.vercel.app", credentials: true })
+  cors({ origin: "https://auth-stuff-mu.vercel.app", credentials: true })
 );
 
 app.use(express.json());
